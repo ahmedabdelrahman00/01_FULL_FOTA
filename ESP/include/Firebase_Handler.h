ad5f356a13bcddd0803407c239431577a01e9694
ahmedabdelrahman00 <93540834+ahmedabdelrahman00@users.noreply.github.com>
2024-04-05 16:31:55 +0200

    add new firebase

--- ESP/include/Firebase_Handler.h
+++ ESP/include/Firebase_Handler.h
@@ -7,16 +7,16 @@
 #define WIFI_SSID             "Ahmeeeed_01"  /* Mi Hunters 74 */
 #define WIFI_PASSWORD         "A369540020a$@Oo" /* eslam2000 _Hunter_007 */
 //Define the API Key
-#define API_KEY               "AIzaSyDdXhbBrDCZwZ08M9oOJKfy_7_05-s9TbY"
-#define DATABASE_URL          "https://update-f19b4-default-rtdb.firebaseio.com/"
+#define API_KEY               "AIzaSyADCXJAtMSX0Fi8jtgz90yF-DepnijSogE"
+#define DATABASE_URL          "https://full-fota-default-rtdb.firebaseio.com/"
  
 //Define the user Email and password that alreadey registerd or added in your project
-#define USER_EMAIL            "ahmedabdelraahmaan@gmail.com"
+#define USER_EMAIL            "ebrahem.mostafa.1812@gmail.com"
 
 #define USER_PASSWORD         "123456789"
 
 //Define the Firebase storage bucket ID e.g bucket-name.appspot.com 
-#define STORAGE_BUCKET_ID     "update-f19b4.appspot.com"
+#define STORAGE_BUCKET_ID     "full-fota.appspot.com" 
 #define BAUD_RATE              9600
 #define RXD2                   16
 #define TXD2                   17
